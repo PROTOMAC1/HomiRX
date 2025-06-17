8a8cbeecb5329564ffdd72537cb87545fdc297ae
Ajinkya Sasne <87075239+PROTOMAC1@users.noreply.github.com>
2025-06-17 14:04:07 +0530

    this is the new animation 1 file

--- real-estate-app/src/components/Propertie.js
+++ real-estate-app/src/components/Propertie.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Propertieimg1 from './Images/Properties1.jpg';
 import Propertieimg2 from './Images/Properties2.jpg';
 import Propertieimg3 from './Images/Properties3.jpg';
@@ -7,22 +7,53 @@ import Propertieimg5 from './Images/Properties5.jpg';
 import Propertieimg6 from './Images/Properties6.jpg';
 import './Propertie.css';
 
-const Propertie = () => {
-  return (
-    <div className='body'>
-      <div className='gallery'>
-        <div className='gallery-container'>
-          <img className='gallery-item gallery-item-1' src={Propertieimg1} data-index='1' />
-          <img className='gallery-item gallery-item-2' src={Propertieimg2} data-index='2' />
-          <img className='gallery-item gallery-item-3' src={Propertieimg3} data-index='3' />
-          <img className='gallery-item gallery-item-4' src={Propertieimg4} data-index='4' />
-          <img className='gallery-item gallery-item-5' src={Propertieimg5} data-index='5' />
-          <img className='gallery-item gallery-item-6' src={Propertieimg6} data-index='6' />
-        </div>
-        <div className='gallery-controls'></div>
-      </div>
-    </div> 
-  )
-}
+const images = [
+  Propertieimg1,
+  Propertieimg2,
+  Propertieimg3,
+  Propertieimg4,
+  Propertieimg5,
+  Propertieimg6,
+];
 
-export default Propertie
+const Propertie = () => {
+  const [carouselItems, setCarouselItems] = useState(images);
+
+  const handlePrevious = () => {
+    const updated = [carouselItems[carouselItems.length - 1], ...carouselItems.slice(0, -1)];
+    setCarouselItems(updated);
+  };
+
+  const handleNext = () => {
+    const updated = [...carouselItems.slice(1), carouselItems[0]];
+    setCarouselItems(updated);
+  };
+
+  return (
+    <div className="body">
+      <div className="gallery">
+        <div className="gallery-container">
+          {carouselItems.map((img, index) => (
+            <img
+              key={index}
+              src={img}
+              alt={`Property ${index + 1}`}
+              className={`gallery-item gallery-item-${index + 1}`}
+              data-index={index + 1}
+            />
+          ))}
+        </div>
+        <div className="gallery-controls">
+          <button className="gallery-controls-previous" onClick={handlePrevious}>
+            Previous
+          </button>
+          <button className="gallery-controls-next" onClick={handleNext}>
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Propertie;
